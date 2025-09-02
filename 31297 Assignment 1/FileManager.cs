1862c3650f146002dd677dac27ad3a8ca9622e67
25331859 <25331859@LAB11B14V1012>
2025-09-02 13:30:36 +1000

    FileManager

---
+++ 31297 Assignment 1/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Numerics;
+using static AssignmentApp.User;
+
+namespace AssignmentApp
+{
+    static class FileManager
+    {
+        private ArrayList<User> users = new ArrayList<User>();
+        public string InitialiseUsers(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            while (!sr.EndOfStream) 
+            {
+                string line = sr.ReadLine();
+                if (u[0] == "Admin")
+                {
+                    Admin a = new Admin();
+                    a.LoadUser(line);
+                    users.Add(a);
+                }
+
+            }
+
+        }
+
+    }
+}
